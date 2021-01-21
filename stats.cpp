f1a105cbae794e3b735ab480ed0efacd4f3437dd
Arun Raveendran <arunr4957@gmail.com>
2021-01-21 15:39:35 +0530

    Update stats.cpp
    Reverting the changes.

--- stats.cpp
+++ stats.cpp
@@ -1,10 +1,4 @@
 #include "stats.h"
-typedef struct _stats
-{
-    int average;
-    int min;
-    int max
-}Stats;
 
 Stats Statistics::ComputeStatistics(const std::vector<___>& ) {
     //Implement statistics here
