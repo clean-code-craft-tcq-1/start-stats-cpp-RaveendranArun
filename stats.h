5adc54e5a5e34d89800d7042b3552886c5dbd918
Arun Raveendran <arunr4957@gmail.com>
2021-01-21 15:39:56 +0530

    Update stats.h
    Adding definition for Stats.

--- stats.h
+++ stats.h
@@ -1,5 +1,12 @@
 #include <vector>
 
+typedef struct _stats
+{
+    int average;
+    int min;
+    int max
+}Stats;
+
 namespace Statistics {
     Stats ComputeStatistics(const std::vector<___>& );
 }
