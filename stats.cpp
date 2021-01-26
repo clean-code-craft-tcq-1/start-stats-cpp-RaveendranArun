d77c3179b8d2cb992e1c248516f3e8c299800821
Arun Raveendran <arunr4957@gmail.com>
2021-01-26 10:17:00 +0530

    Update stats.cpp
    Find the average of received vector.

--- stats.cpp
+++ stats.cpp
@@ -1,5 +1,14 @@
 #include "stats.h"
 
-Stats Statistics::ComputeStatistics(const std::vector<___>& ) {
-    //Implement statistics here
+Stats Statistics::ComputeStatistics(const std::vector<float>& val) {
+    
+    Stats retStats;
+    float sum = 0;
+    
+    /* Find the avarage */
+    for (auto i = val.begin(); i != val.end(); ++i)
+        sum += *i;
+    retStats.average = sum / (float)val.size();
+    
+    return retStats;
 }
